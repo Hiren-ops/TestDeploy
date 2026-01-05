8afce7f58637d51ec64422dae518cc15092f557e
Hiren-ops <hiren@onov8.com>
2026-01-06 02:05:41 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = exoress();
+
+app.get('/', req, res) => {
+  res.send('Hello from CodeDeploy + Docker!');
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
