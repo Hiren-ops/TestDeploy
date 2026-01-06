443e46fb47f6664bebe0476d5ef8484be8df8099
Hiren-ops <hiren@onov8.com>
2026-01-06 17:14:31 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+
+AWS_REGION=me-central-1
+ACCOUNT_ID=767397902739
+IMAGE_NAME=sample-app
+IMAGE_TAG=latest
+
+aws ecr get-login-password --region $AWS_REGION | \
+docker login --username AWS --password-stdin \
+$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
+
 docker stop sample-app||true
 docker rm sample-app||true
 
-docker build -t sample-app /home/ec2-user/app
-docker run -d -p 80:3000 --name sample-app sample-app
+docker run -d \
+  --name sample-app \
+  -p 80:3000 \
+  $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
